47a80e2dd499a669a4550c4047fb551693d994b3
Andre Pierre <andre.laurence94@gmail.com>
2020-07-29 21:49:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const shout = (string) => {
   return string.toUpperCase()
 }
+
+const whisper = (string) => {
+  return string.toLowerCase()
+}
+
+const logShout = (string) => {
+  let shout = string.toUpperCase()
+  console.log(shout)
+}
