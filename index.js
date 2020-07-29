1877ea832f31821f1d3b7c0dbc3f6eaddfc88584
Andre Pierre <andre.laurence94@gmail.com>
2020-07-29 21:55:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const shout = (string) => {
   return string.toUpperCase();
-}
+};
 
 const whisper = (string) => {
   return string.toLowerCase();
-}
+};
 
 const logShout = (string) => {
   let shout = string.toUpperCase();
   console.log(shout)
-}
+};
 
 const logWhisper = (string) => {
   let whisp = string.toLowerCase();
   console.log(whisp);
 };
+
+const sayHiToGrandma = (string) => {
+  if (string === string.toLowerCase()) {
+    return 'I can\'t hear you!'
+  }
+}
